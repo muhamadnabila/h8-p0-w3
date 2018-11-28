f65ad3122a5384a1e7a6db4005fa8bfc12c92353
bilal <mbilalghz@gmail.com>
2018-11-28 21:01:58 +0700

    exercise-2

--- exercise-2/exercise2.js
+++ exercise-2/exercise2.js
@@ -1,8 +1,7 @@
 function balikString(kata){
 
-    var a = kata.length;
     var b = '';
-    for (a; a >= 1 ; a--){
+    for (var a= kata.length; a >= 1 ; a--){
         b = b + kata[a-1]
     }return b
 
