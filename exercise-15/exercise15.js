6c1115f31bbdfcd6790f46b36712b71e7747026a
bilal <mbilalghz@gmail.com>
2018-12-17 12:02:20 +0700

    exercise-15

--- exercise-15/exercise15.js
+++ exercise-15/exercise15.js
@@ -5,7 +5,7 @@ function groupAnimals(animals) {
     var result = []
     for ( var i = 0; i < kamus.length; i++){
         for ( var j = 0; j < animals.length; j++){ 
-            if (animals[j][0] === kamus[i]){
+            if (animals[j][0] === kamus[i][0]){
                   temp.push(animals[j])
             }  
         }
@@ -18,7 +18,7 @@ function groupAnimals(animals) {
 }
   
   // TEST CASES
-  console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil']));
+  console.log(groupAnimals(['facing', 'ayam', 'kuda', 'anoa', 'kancil']));
   // [ ['ayam', 'anoa'], ['cacing'], ['kuda', 'kancil'] ]
   console.log(groupAnimals(['cacing', 'ayam', 'kuda', 'anoa', 'kancil', 'unta', 'cicak' ]));
   // [ ['ayam', 'anoa'], ['cacing', 'cicak'], ['kuda'], ['unta'] ]
