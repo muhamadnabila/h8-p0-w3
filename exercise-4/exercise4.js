db82949d3c1018ae8931cd1c40a6b155213ac482
bilal <mbilalghz@gmail.com>
2018-12-17 11:56:48 +0700

    exercise-4

--- exercise-4/exercise4.js
+++ exercise-4/exercise4.js
@@ -1,72 +1,73 @@
 function dataHandling2(input){
-    
-    input.splice(1,1,"Roman Alamsyah Elsharawy")
-    input.splice(2,1,'Provinsi Bandar Lampung')
-    input.splice(4,2,"Pria","SMA Internasional Metro")
-   
-    console.log(input)
-
-    var splitKata = input[3].split("/")
-    
-    
-    switch(splitKata[1]){
-        case "01" :
-           console.log ('Januari');
-           break;
-        case "02" :
-            console.log ('Februari')
-            break;
-        case "03" :
-            console.log ('Maret');
-            break;
-        case "04" :
-             console.log ('April')
-             break;
-        case "05" :
-           console.log ('Mei');
-           break;
-        case "06" :
-            console.log ('Juni')
-            break;
-        case "07" :
-           console.log ('Juli');
-           break;
-        case "08" :
-            console.log ('Agustus')
-            break;
-        case "09" :
-           console.log ('September');
-           break;
-        case "10" :
-            console.log ('Oktober')
-            break;
-        case "11" :
-           console.log ('November');
-           break;
-        case "12" :
-            console.log ('Desember');
-            break;
-        default :
-            console.log('Salah bulan');
-            break;
+    input.splice(1, 2);
+    input.splice(1, 0, 'Roman Alamsyah Elsharawy', 'Provinsi Bandar Lampung'); 
+    input.splice(4, 1);
+    input.splice(4, 0, 'Pria', 'SMA Internasional Metro');
+    console.log (input);
+    var ambiltanggal;
+    ambiltanggal = input.slice(3,4) + '';
+    ambiltanggal = ambiltanggal.split('/');
+    var namaBulan=ambiltanggal[1];
+    switch (namaBulan){
+        case '01':
+        namaBulan = 'Januari';
+        break;
+        case '02':
+        namaBulan = 'Februari';
+        break;
+        case '03':
+        namaBulan = 'Maret';
+        break;
+        case '04':
+        namaBulan = 'April';
+        break;
+        case '05':
+        namaBulan = 'Mei';
+        break;
+        case '06':
+        namaBulan = 'Juni';
+        break;
+        case '07':
+        namaBulan = 'Juli';
+        break;
+        case '08':
+        namaBulan = 'Agustus';
+        break;
+        case '09':
+        namaBulan = 'September';
+        break;
+        case '10':
+        namaBulan = 'Oktober';
+        break;
+        case '11':
+        namaBulan = 'November';
+        break;
+        case '12':
+        namaBulan = 'Desember';
+        break;
+        default:;
     }
-   
-    
-
-    var sortingInput = splitKata;
-    sortingInput.sort(function(value1,value2){return value2 - value1})
-    console.log(sortingInput);
-      
-    console.log(splitKata.join(' - '))
-
-    var irisanInput = input[1].slice(0,15)
-    console.log(irisanInput)
-   
-    
-  
-    
+    console.log(namaBulan);
+    var susuntanggal = ambiltanggal;
+    susuntanggal.sort(function(value1,value2){return Number(value1) < Number(value2)});
+    console.log (susuntanggal);
+    ambiltanggal = input.slice(3,4) + '';
+    ambiltanggal = ambiltanggal.split('/');
+var jointanggal = ambiltanggal
+    console.log (jointanggal.join('-'));
+    var nama = input[1];
+    console.log (nama.slice(0,15));
 }
 
-var input = ["0001", "Roman Alamsyah", "Bandar Lampung", "21/05/1989", "Membaca"]  
+var input = ['0001', 'Roman Alamsyah ', 'Bandar Lampung', '21/05/1989', 'Membaca'];
 dataHandling2(input);
 
+/**
+ * keluaran yang diharapkan (pada console)
+ *
+ * ["0001", "Roman Alamsyah Elsharawy", "Provinsi Bandar Lampung", "21/05/1989", "Pria", "SMA Internasional Metro"]
+ * Mei
+ * ["1989", "21", "05"]
+ * 21-05-1989
+ * Roman Alamsyah
+*/
